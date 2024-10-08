7fc6684384e152af2c4e5b340f2f88c2be87fab7
IvanSlivenko <ivan8822@ukr.net>
2024-10-08 16:58:22 +0300

    08-10-2024-16-58

--- index.js
+++ index.js
@@ -1,3 +1,26 @@
 const fs = require('fs');
+const express = require('express');
 
-fs.writeFileSync('info.txt', 'test text');
+// fs.writeFileSync('info.txt', 'test text');
+
+const app = express();
+
+let PORT = 3001;
+
+app.get('/', function (req, res) {
+    // res.send(__dirname + '/templates/index.html');
+    res.sendFile(__dirname + '/templates/index.html');
+
+    // res.end();
+  })
+
+  app.get('/about', function (req, res) {
+    // res.send('About express');
+    res.sendFile(__dirname + '/templates/about.html');
+    // res.end();
+  })  
+  
+  app.listen(PORT , ()=>{
+    console.log(`Старт сервера відбувся : http://localchost:${PORT}`);
+    
+  })
